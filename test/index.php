40e539517799c4ac36086768ab1fe974a1cf1e0f
Viktor <samohavitya@mail.ru>
2021-09-27 12:25:15 +0300

    Создал новый компонент и интегрировал его на страницу "тест"

--- test/index.php
+++ test/index.php
@@ -6,5 +6,12 @@ $APPLICATION->SetPageProperty("keywords","Тестовая страница");
 $APPLICATION->SetPageProperty("description","Тестовая страница");
 ?>
     <p>Тестовая страница, созданная через ИДЕ</p>
+<? $APPLICATION->IncludeComponent(
+    "dv:date.current",
+    ".default",
+    Array(
+    ),
+    false
+);?>
 
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
