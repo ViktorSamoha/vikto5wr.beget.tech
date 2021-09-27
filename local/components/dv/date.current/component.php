57227498daa69844157cfcc24639d1d161a64eb6
Viktor <samohavitya@mail.ru>
2021-09-27 12:36:45 +0300

    Добавил новый функицонал в компонент

--- local/components/dv/date.current/component.php
+++ local/components/dv/date.current/component.php
@@ -1,6 +1,14 @@
 <?php
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 $arResult['DATE'] = date('Y-m-d');
+$file = simplexml_load_file("http://www.cbr.ru/scripts/XML_daily.asp?date_req=".date("d/m/Y"));
+
+$valutes = array();
+
+foreach ($file AS $el){
+    $valutes[strval($el->CharCode)] = strval($el->Value);
+}
+print_r($valutes);
 $this->IncludeComponentTemplate();
 ?>
 
