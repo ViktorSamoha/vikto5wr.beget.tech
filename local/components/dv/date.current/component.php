88b9a73e9c49b3bf21bc59a89ef5636f9c852d0f
Viktor <samohavitya@mail.ru>
2021-09-27 12:39:46 +0300

    Добавил новый функицонал в компонент

--- local/components/dv/date.current/component.php
+++ local/components/dv/date.current/component.php
@@ -1,14 +1,24 @@
 <?php
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 $arResult['DATE'] = date('Y-m-d');
-$file = simplexml_load_file("http://www.cbr.ru/scripts/XML_daily.asp?date_req=".date("d/m/Y"));
 
-$valutes = array();
+$curDate = date('d/m/Y', mktime(0,0,0,date("n"),date("j")+1,date("Y")));
+$curDate2 = date('d/m/Y', mktime(0,0,0,date("n"),date("j")-10,date("Y")));
 
-foreach ($file AS $el){
-    $valutes[strval($el->CharCode)] = strval($el->Value);
+$currencyXML = file_get_contents('http://www.cbr.ru/scripts/XML_dynamic.asp?date_req1='.$curDate2.'&date_req2='.$curDate.'&VAL_NM_RQ=R01235');
+
+$currencyXML = str_replace(Array("\r\n","\n","\r"),'',$currencyXML);
+
+preg_match_all('#<Value>(.*?)</Value>#', $currencyXML, $arValues, PREG_PATTERN_ORDER);
+
+if(count($arValues[1])>=2) {
+
+	$arValues[1] = array_reverse($arValues[1]);
+
+    $arResult['KURS'] = str_replace(",",".",$arValues[1][0]);
+
+    $arResult['DIFF'] = round(floatval(str_replace(",",".",$arValues[1][0])) - floatval(str_replace(",",".",$arValues[1][1])), 4);
 }
-print_r($valutes);
 $this->IncludeComponentTemplate();
 ?>
 
